3f68e283e76adbbf73ede86cca5ac500e97fda8b
Rob Feng <rob.feng@quest.com>
2017-04-26 03:38:04 +0800

    add case 02, have fake class

---
+++ Case 02/02 Fixing the Client API - Dependency Injection/SmartHomeSystem.UnitTest/SmartHomeControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using Moq;
+
+namespace SmartHomeSystem.UnitTest
+{
+    [TestFixture]
+    public class SmartHomeControllerTests
+    {
+        public class FakeDateTimeProvider : IDateTimeProvider
+        {
+            public DateTime ReturnValue { get; set; }
+
+            public DateTime GetDateTime() { return ReturnValue; }
+
+            public FakeDateTimeProvider(DateTime returnValue) { ReturnValue = returnValue; }
+        }
+
+        [Test]
+        public void ActuateLights_MotionDetectedAtNight_TurnsOnTheLight()
+        {
+            // Arrange
+            var controller = new SmartHomeController(new FakeDateTimeProvider(new DateTime(2015, 12, 31, 23, 59, 59)));
+
+            // Act
+            controller.ActuateLights(true);
+
+            // Assert
+            Assert.AreEqual(new DateTime(2015, 12, 31, 23, 59, 59), controller.LastMotionTime);
+        }
+
+        [Test]
+        public void ActuateLights_MotionNotDetected_NoChangeTimeOfMotion()
+        {
+            // Arrange
+            var fakeDateTimeProvider = Mock.Of<IDateTimeProvider>();
+            var controller = new SmartHomeController(fakeDateTimeProvider);
+            
+            // Act
+            controller.ActuateLights(false);
+
+            // Assert
+            Assert.AreEqual(new DateTime(0), controller.LastMotionTime);
+        }
+    }
+}
---
+++ Case 02/02 Fixing the Client API - Dependency Injection/SmartHomeSystem/SmartHomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeSystem
+{
+    /// <summary>
+    /// Code from https://www.toptal.com/qa/how-to-write-testable-code-and-why-it-matters
+    /// </summary>
+    public class SmartHomeController
+    {
+        private readonly IDateTimeProvider FDateTimeProvider; // Dependency
+
+        public DateTime LastMotionTime { get; private set; }
+
+        public SmartHomeController(IDateTimeProvider dateTimeProvider)
+        {
+            // Inject required dependency in the constructor.
+            FDateTimeProvider = dateTimeProvider;
+        }
+
+        public void ActuateLights(bool motionDetected)
+        {
+            DateTime time = FDateTimeProvider.GetDateTime(); // Delegating the responsibility
+
+            // Update the time of last motion.
+            if (motionDetected)
+            {
+                LastMotionTime = time;
+            }
+
+            // If motion was detected in the evening or at night, turn the light on.
+            string timeOfDay = GetTimeOfDay(time);
+            if (motionDetected && (timeOfDay == "Evening" || timeOfDay == "Night"))
+            {
+                BackyardLightSwitcher.Instance.TurnOn();
+            }
+            // If no motion is detected for one minute, or if it is morning or day, turn the light off.
+            else if (time.Subtract(LastMotionTime) > TimeSpan.FromMinutes(1) || (timeOfDay == "Morning" || timeOfDay == "Noon"))
+            {
+                BackyardLightSwitcher.Instance.TurnOff();
+            }
+        }
+
+        public string GetTimeOfDay(DateTime dateTime)
+        {
+            if (dateTime.Hour >= 0 && dateTime.Hour < 6)
+            {
+                return "Night";
+            }
+            if (dateTime.Hour >= 6 && dateTime.Hour < 12)
+            {
+                return "Morning";
+            }
+            if (dateTime.Hour >= 12 && dateTime.Hour < 18)
+            {
+                return "Noon";
+            }
+            return "Evening";
+        }
+    }
+}
