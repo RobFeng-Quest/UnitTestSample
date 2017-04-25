c95a88ae588e5ee430558d969de5a7b0eae5fa5f
Rob Feng <rob.feng@quest.com>
2017-04-26 02:01:20 +0800

    Add Case 01 - UnderstandCodeTest

---
+++ Case 01/UnderstandCodeTest/UnderstandCodeTest/SimpleParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandCodeTest
+{
+    public class SimpleParser
+    {
+        public int ParseAndSum(string aNumbers)
+        {
+            if (aNumbers.Length == 0)
+            {
+                return 0;
+            }
+
+            if (!aNumbers.Contains(","))
+            {
+                return int.Parse(aNumbers);
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    "I can only handle 0 or 1 number for now!");
+            }
+        }
+    }
+}
---
+++ Case 01/UnderstandCodeTest/UnderstandCodeTest/SimpleParserTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandCodeTest
+{
+    public class SimpleParserTests
+    {
+        public static void ParseAndSum_ReturnZeroWhenEmptyString()
+        {
+            try
+            {
+                SimpleParser parser = new SimpleParser();
+
+                int result = parser.ParseAndSum(string.Empty);
+
+                if (result != 1)
+                {
+                    Console.WriteLine(
+                        @"---SimpleParserTests.ParseAndSum_ReturnZeroWhenEmptyString:" + Environment.NewLine
+                        + "Parse and sum should have returned 0 on an empty string" + Environment.NewLine
+                        + "--------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static void ParseAndSum_Return3When3String()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            try
+            {
+                SimpleParser parser = new SimpleParser();
+
+                int result = parser.ParseAndSum("4");
+
+                if (result != 3)
+                {
+                    ShowProblem(testName, "Parse and sum should have returned 3 on an 3 string");
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowProblem(testName, ex.ToString());
+            }
+        }
+
+        public static void ShowProblem(string test, string message)
+        {
+            string msg = string.Format(
+                @"---{0}---" + Environment.NewLine
+                + "{1}" + Environment.NewLine
+                + "--------------------", test, message);
+            Console.WriteLine(msg);
+        }
+    }
+}
